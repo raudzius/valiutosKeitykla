7ca6018af3fb4b551475d8f77faa0a74b4ac7bdf
Mykolas Raudžius <s063337@ad.viko.lt>
2026-02-12 13:51:17 +0200

    inicijuotas projektas

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+double apdorotiValiuta(double valiutosIndexas, double kiekis)
+{
+    return valiutosIndexas * kiekis;
+}
+
+int main() {
+    // Valiutų kursai
+    double GBP_Bendras   = 0.8729;
+    double GBP_Pirkti    = 0.8600;
+    double GBP_Parduoti  = 0.9220;
+    double USD_Bendras   = 1.1793;
+    double USD_Pirkti    = 1.1460;
+    double USD_Parduoti  = 1.2340;
+    double INR_Bendras   = 104.6918;
+    double INR_Pirkti    = 101.3862;
+    double INR_Parduoti  = 107.8546;
+
+string norimaPaslauga = "";
+    string valiuta="";
+    double valiutosKiekis=0;
+
+    cout << "Pasirinkite norima paslauga, parasydami teksta kabutese" << endl;
+    cout << "Valiutos kurso palyginimas su kita valiuta. \"palyginimas\"" << endl;
+    cout << "Galimybė įsigyti valiutos (t. y. pirkti valiutą). \"pirkti\"" << endl;
+    cout << "Galimybė parduoti valiutą (t. y. parduoti valiutą). \"parduoti\"" << endl;
+
+    cin>>norimaPaslauga;
+    switch (norimaPaslauga) {
+        case "palyginimas":
+        cout << "Su kokia valiuta norite palyginti? \"GBP\", \"USD\", \"INR\"" << endl;
+        cin >> valiuta;
+        cout << "Valiutos Kiekis? pvz: \"5.12\"";
+        cin>>valiutosKiekis;
+
+        switch (valiuta) {
+            case "GBP":
+                cout << apdorotiValiuta(GBP_Bendras,valiutosKiekis) <<endl;
+            break;
+            case "USD":
+                cout << apdorotiValiuta(USD_Bendras,valiutosKiekis) <<endl;
+            break;
+case "INR":
+    cout << apdorotiValiuta(INR_Bendras,valiutosKiekis) <<endl;
+            break;
+        }
+
+        break;
+        case 'pirkti':
+            cout << "Kokia valiuta norite pirkti? \"GBP\", \"USD\", \"INR\"" << endl;
+        cin >> valiuta;
+        cout << "Valiutos Kiekis? pvz: \"5.12\"";
+        cin>>valiutosKiekis;
+        switch (valiuta) {
+            case 'GBP':
+                cout << apdorotiValiuta(GBP_Pirkti,valiutosKiekis) <<endl;
+            case 'USD':
+                cout << apdorotiValiuta(USD_Pirkti,valiutosKiekis) <<endl;
+            case 'INR':
+                cout << apdorotiValiuta(INR_Pirkti,valiutosKiekis) <<endl;
+        }
+        break;
+        case 'parduoti':
+            cout << "Kokia valiuta norite parduoti? \"GBP\", \"USD\", \"INR\"" << endl;
+        cin >> valiuta;
+        cout << "Valiutos Kiekis? pvz: \"5.12\"";
+        cin>>valiutosKiekis;
+        switch (valiuta) {
+            case 'GBP':
+                cout << apdorotiValiuta(GBP_Parduoti,valiutosKiekis) <<endl;
+            case 'USD':
+                cout << apdorotiValiuta(USD_Parduoti,valiutosKiekis) <<endl;
+            case 'INR':
+                cout << apdorotiValiuta(INR_Parduoti,valiutosKiekis) <<endl;
+        }
+        default:
+            return 0;
+    }
+    return 0;
+}
